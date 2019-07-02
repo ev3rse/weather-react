32885916c30bc263e8e31a6e23aebf80f4117f72
ev3rse <ev3rse@yandex.ru>
2019-07-02 13:58:07 +0700

    Init commit.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<div className="App-Search">
+					Search
+				</div>
+				<div className="App-Weather">
+					Weather
+				</div>
+			</div>
+		)
+	};
+}
+
+export default App;
